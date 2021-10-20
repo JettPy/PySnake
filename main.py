abd61f8c8a32a683616120394df82294cd9d1ce7
Sergey Melnikov <jettpy@yandex.ru>
2021-10-20 03:32:55 +0300

    fix: исправить баги и оптимизировать запуск

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 import sys
 from components.Game import Game
 
-try:
-    size = int(sys.argv[1])
-except ValueError:
-    print('Invalid parameter. Starting in default mode')
-    size = 30
-
-if size <= 0:
-    print('Invalid parameter. Starting in default mode')
-    size = 30
-
 dark_mode = False
 loop_mode = False
 god_mode = False
+first_time = True
+size = 30
 
-if len(sys.argv) > 2:
-    for i in range(2, len(sys.argv)):
-        if sys.argv[i] == 'd':
+if len(sys.argv) > 1:
+    for i in range(1, len(sys.argv)):
+        if sys.argv[i].isdigit() and first_time:
+            if int(sys.argv[i]) > 0:
+                size = int(sys.argv[i])
+                first_time = False
+        elif sys.argv[i] == 'd':
             dark_mode = True
         elif sys.argv[i] == 'i':
             loop_mode = True
         elif sys.argv[i] == 'g':
             god_mode = True
         else:
-            print('Invalid parametr. Use \"d\" for dark mode or \"i\" for loop mode or \"g\" for god mode')
+            print('Invalid parametr. Use natural numerals for the field size, \"d\" for dark mode, \"i\" for loop mode or \"g\" for god mode')
+
+if size > 45:
+    print('Size is too large')
+    size = 30
 
 try:
     py_snake = Game(size, loop_mode, dark_mode, god_mode)
